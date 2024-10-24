f92097624292586ecdce90f7c4c535b3c2ea09b2
elitemassagemx <elitemassagemx@gmail.com>
2024-10-24 00:00:14 -0600

    Delete config directory

--- config/dependencies.js
+++
@@ -1,9 +0,0 @@
-// config/dependencies.js
-export const DEPENDENCIES = {
-    GSAP: typeof gsap !== 'undefined',
-    ScrollTrigger: typeof ScrollTrigger !== 'undefined',
-    ResizeObserver: typeof ResizeObserver !== 'undefined',
-    IntersectionObserver: typeof IntersectionObserver !== 'undefined',
-    PointerEvents: typeof PointerEvent !== 'undefined',
-    TouchEvents: 'ontouchstart' in window
-};
